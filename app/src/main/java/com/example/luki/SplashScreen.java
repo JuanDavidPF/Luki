343e4d41fd8e583eb471b37a0a057bc3d25126bc
JuanDavidPF <JuanFavidPF@gmail.com>
2020-10-09 14:52:17 -0500

    Finished the splash screen
    Motion layout animated splash screen

---
+++ app/src/main/java/com/example/luki/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.example.luki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+
+
+
+
+}//closes the SplashCreen class
